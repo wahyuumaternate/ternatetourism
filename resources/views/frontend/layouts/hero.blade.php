232d9e92349cf816404e92609c269b33124f9217
wahyuumaternate <retmujago@gmail.com>
2025-01-07 02:08:15 +0900

    hero

--- resources/views/frontend/layouts/hero.blade.php
+++ resources/views/frontend/layouts/hero.blade.php
@@ -1,30 +1,29 @@
- <section class="hero">
-     <!-- Overlay -->
-     <div class="hero-overlay"></div>
-     <!-- Video Background -->
-     <video autoplay muted loop playsinline class="video-background">
-         <source src="vidio/1228.mp4" type="video/mp4" />
-         Your browser does not support the video tag.
-     </video>
-     <!-- <img
-                src="vidio/1228.gif"
-                alt="Background GIF"
-                class="video-background"
-            /> -->
-     <!-- Content -->
-     <div class="hero-content">
-         <p>Temukan Pesonanya</p>
-         <h1>Kota Ternate, Surga di Timur</h1>
-     </div>
+<section class="hero">
+    <!-- Overlay -->
+    <div class="hero-overlay"></div>
+    <!-- Video Background -->
+    <video autoplay muted loop playsinline class="video-background">
+        <source src="{{ asset('assets/1228.mp4') }}" type="video/mp4" />
+        Your browser does not support the video tag.
+    </video>
+    <!-- <img
+            src="vidio/1228.gif"
+            alt="Background GIF"
+            class="video-background"
+        /> -->
+    <!-- Content -->
+    <div class="hero-content">
+        <p>Temukan Pesonanya</p>
+        <h1>Kota Ternate, Surga di Timur</h1>
+    </div>
 
-     <!-- Logos -->
-     <div class="logos">
-         <!-- <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/7/75/Lambang_Kota_Ternate.png"
-                    alt="UNWTO"
-                /> -->
-         <img src="vidio/Pesona-Indonesia-Logo-Single-Color-Version-White.png" alt="Wonderful Indonesia"
-             width="150" />
-         <img src="vidio/Logo_WI_Final_20092017_WHITE_9a2b7d834c.png" alt="Wonderful Indonesia" />
-     </div>
- </section>
+    <!-- Logos -->
+    <div class="logos">
+        <!-- <img
+                src="https://upload.wikimedia.org/wikipedia/commons/7/75/Lambang_Kota_Ternate.png"
+                alt="UNWTO"
+            /> -->
+        <img src="{{ asset('assets/ternate tourims.png') }}" alt="Wonderful Indonesia" width="150" />
+        <img src="{{ asset('assets/Logo_WI_Final_20092017_WHITE_9a2b7d834c.png') }}" alt="Wonderful Indonesia" />
+    </div>
+</section>
