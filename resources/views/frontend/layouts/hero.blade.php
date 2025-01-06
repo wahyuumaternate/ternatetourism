5b378272794a58388c73e95a9868a9b6e5b9ba2d
wahyuumaternate <retmujago@gmail.com>
2025-01-07 02:04:06 +0900

    push

--- resources/views/frontend/layouts/hero.blade.php
+++ resources/views/frontend/layouts/hero.blade.php
@@ -2,15 +2,12 @@
     <!-- Overlay -->
     <div class="hero-overlay"></div>
     <!-- Video Background -->
-    <video autoplay muted loop playsinline class="video-background">
+    <video autoplay muted loop playsinline preload="auto" class="video-background">
         <source src="{{ asset('assets/1228.mp4') }}" type="video/mp4" />
         Your browser does not support the video tag.
     </video>
-    <!-- <img
-            src="vidio/1228.gif"
-            alt="Background GIF"
-            class="video-background"
-        /> -->
+    <!-- Fallback Image -->
+    <img src="{{ asset('assets/1228.gif') }}" alt="Background GIF" class="video-background" />
     <!-- Content -->
     <div class="hero-content">
         <p>Temukan Pesonanya</p>
@@ -19,10 +16,6 @@
 
     <!-- Logos -->
     <div class="logos">
-        <!-- <img
-                src="https://upload.wikimedia.org/wikipedia/commons/7/75/Lambang_Kota_Ternate.png"
-                alt="UNWTO"
-            /> -->
         <img src="{{ asset('assets/ternate tourims.png') }}" alt="Wonderful Indonesia" width="150" />
         <img src="{{ asset('assets/Logo_WI_Final_20092017_WHITE_9a2b7d834c.png') }}" alt="Wonderful Indonesia" />
     </div>
