aca996721bb089b7ac2102d8036dc52692b1d8e8
wahyuumaternate <retmujago@gmail.com>
2025-01-07 02:06:05 +0900

    tes

--- resources/views/frontend/layouts/hero.blade.php
+++ resources/views/frontend/layouts/hero.blade.php
@@ -4,11 +4,11 @@
     <!-- Video Background -->
     <video autoplay muted loop playsinline preload="auto" class="video-background">
         <source src="{{ asset('assets/1228.mp4') }}" type="video/mp4" />
+        <img src="{{ asset('assets/1228.gif') }}" alt="Background GIF" class="video-background" />
         Your browser does not support the video tag.
     </video>
-    <!-- Fallback Image -->
-    <img src="{{ asset('assets/1228.gif') }}" alt="Background GIF" class="video-background" />
-    <!-- Content -->
+
+
     <div class="hero-content">
         <p>Temukan Pesonanya</p>
         <h1>Kota Ternate, Surga di Timur</h1>
@@ -16,6 +16,10 @@
 
     <!-- Logos -->
     <div class="logos">
+        <!-- <img
+                src="https://upload.wikimedia.org/wikipedia/commons/7/75/Lambang_Kota_Ternate.png"
+                alt="UNWTO"
+            /> -->
         <img src="{{ asset('assets/ternate tourims.png') }}" alt="Wonderful Indonesia" width="150" />
         <img src="{{ asset('assets/Logo_WI_Final_20092017_WHITE_9a2b7d834c.png') }}" alt="Wonderful Indonesia" />
     </div>
