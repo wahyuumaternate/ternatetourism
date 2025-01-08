4b2bad9ad291b1bc884c35e4fa93ad0ee4f563d4
wahyuumaternate <retmujago@gmail.com>
2025-01-08 21:23:03 +0900

    favicon

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -9,6 +9,10 @@
     <meta content="" name="description">
     <meta content="" name="keywords">
 
+    {{-- favicon --}}
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    
     <!-- Favicons -->
     <link href="{{ asset('front/img/logo-unkhair.png') }}" rel="icon">
     <link href="{{ asset('front/img/logo-unkhair.png') }}" rel="apple-touch-icon">
--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -9,6 +9,10 @@
     @endif
     @stack('meta')
 
+    {{-- favicon --}}
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <!-- Fancyapps CSS -->
