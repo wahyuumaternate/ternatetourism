0b81612c564b9a884b84858e801674cedf68a170
wahyuumaternate <retmujago@gmail.com>
2025-01-23 22:50:05 +0900

    done

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -35,7 +35,7 @@
                             </div>
 
                             <div class="card-body">
-                                <h5 class="card-title">Pengunjung <span>| Hari Ini</span></h5>
+                                <h5 class="card-title">Pengunjung <span></span></h5>
                                 <div class="d-flex align-items-center">
                                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                         <i class="bi bi-people"></i>
@@ -54,7 +54,7 @@
             <div class="col-12">
                 <div class="card recent-sales overflow-auto">
                     <div class="card-body">
-                        <h5 class="card-title">Data Pengunjung <span>| Hari Ini</span></h5>
+                        <h5 class="card-title">Data Pengunjung <span></span></h5>
                         <table class="table table-borderless datatable">
                             <thead>
                                 <tr>
