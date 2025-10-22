65276f43c9366171bc14593f622e141d4746f582
wahyuumaternate <retmujago@gmail.com>
2025-10-22 12:01:44 +0900

    quote

--- resources/lang/en/quotes.php
+++ resources/lang/en/quotes.php
@@ -1,26 +1,30 @@
 <?php
 return [
     'q1' => [
+        'text' => "Wherever you stand, be the soul of that place.",
+        'author' => "Jalaluddin Rumi",
+    ],
+    'q2' => [
         'text' => "Travel makes one modest, you see what a tiny place you occupy in the world.",
         'author' => "Gustave Flaubert",
     ],
-    'q2' => [
+    'q3' => [
         'text' => "Traveling—it leaves you speechless, then turns you into a storyteller.",
         'author' => "David Attenborough",
     ],
-    'q3' => [
+    'q4' => [
         'text' => "The real voyage of discovery consists not in seeking new landscapes but in having new eyes.",
         'author' => "Marcel Proust",
     ],
-    'q4' => [
+    'q5' => [
         'text' => "The impulse to travel is one of the hopeful symptoms of life.",
         'author' => "Mark Twain",
     ],
-    'q5' => [
+    'q6' => [
         'text' => "Adventure is worthwhile.",
         'author' => "Aristotle",
     ],
-    'q6' => [
+    'q7' => [
         'text' => "The world is a book and those who do not travel read only one page.",
         'author' => "Saint Augustine",
     ],
--- resources/lang/id/quotes.php
+++ resources/lang/id/quotes.php
@@ -1,26 +1,29 @@
-<?php
-return [
+<?php return [
     'q1' => [
+        'text' => "Di mana pun kamu berada, jadilah jiwa di tempat itu.",
+        'author' => "Jalaluddin Rumi",
+    ],
+    'q2' => [
         'text' => "Perjalanan membuat seseorang rendah hati, kamu sadar betapa kecilnya tempatmu di dunia.",
         'author' => "Gustave Flaubert",
     ],
-    'q2' => [
+    'q3' => [
         'text' => "Bepergian membuatmu terdiam, lalu menjadikanmu seorang pendongeng.",
         'author' => "David Attenborough",
     ],
-    'q3' => [
+    'q4' => [
         'text' => "Perjalanan sejati bukanlah mencari pemandangan baru, melainkan memiliki mata yang baru.",
         'author' => "Marcel Proust",
     ],
-    'q4' => [
+    'q5' => [
         'text' => "Dorongan untuk bepergian adalah salah satu gejala penuh harapan dari kehidupan.",
         'author' => "Mark Twain",
     ],
-    'q5' => [
+    'q6' => [
         'text' => "Petualangan itu berharga.",
         'author' => "Aristoteles",
     ],
-    'q6' => [
+    'q7' => [
         'text' => "Dunia adalah sebuah buku, dan mereka yang tidak bepergian hanya membaca satu halaman.",
         'author' => "Santo Agustinus",
     ],
